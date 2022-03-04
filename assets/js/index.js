9f1521e6ce70f772a16ed744b7bd37a552ef7fa7
YYY <crosspool03@gmail.com>
2022-03-04 11:33:38 -0300

    update

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+const buttons = document.querySelectorAll('button');
+const image = document.querySelectorAll('img');
+const p = document.querySelectorAll('p');
+
+const musics = [
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOOCXhI9T4YdreEByvjrCpvPsigy-H-ZTtCA&usqp=CAU",
+    nome: "Disaster",
+    music: ""
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjzSjn8hQD0TvzznW25Z4lmgLblq0NbxpREA&usqp=CAU",
+    nome: "Фонк для бега",
+    music: ""
+  },
+  {
+    img: "https://i.pinimg.com/736x/e5/19/4b/e5194b3ae40a64205bbc7ed5c5fc55d5.jpg",
+    nome: "Feel Like God",
+    music: ""
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy7a6lpZlN1ylWRALHjPTE0YbU7SSDMT06KA&usqp=CAU",
+    nome: "After Dark",
+    music: ""
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvaC4xcBKtvpakF6oJDQ7aTjK_1vagv72Ikg&usqp=CAU",
+    nome: "Chase",
+    music: ""
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFgszZJTMgXz73HdUokiP8_MWoud1qkLyiPw&usqp=CAU",
+    nome: "All Alone",
+    music: ""
+  }
+];
+
+
+musics.forEach((music) => {
+  console.log(music.nome)
+});
+
+
+
+
